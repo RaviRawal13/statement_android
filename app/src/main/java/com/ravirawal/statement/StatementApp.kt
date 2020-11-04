03c0bbabbbc18f3f3b494da27031acfb4bdfb8f5
ravirawal13 <ravirawal13@gmail.com>
2020-11-05 02:27:58 +0530

    activities Main and splash added

---
+++ app/src/main/java/com/ravirawal/statement/StatementApp.kt
@@ -0,0 +1,30 @@
+package com.ravirawal.statement
+
+import android.app.Application
+import com.ravirawal.statement.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class StatementApp : Application(), HasAndroidInjector {
+
+    @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun onCreate() {
+        super.onCreate()
+        initDagger()
+    }
+
+    private fun initDagger() {
+        DaggerAppComponent
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
+    }
+}
